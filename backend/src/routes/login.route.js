d8cab7690d8eaa51f800e8e514e9c0077f297611
Wil Treasure <wiltreasure@gmail.com>
2023-03-25 20:23:34 +0000

    JWT Authentication added

--- backend/src/routes/login.route.js
+++ backend/src/routes/login.route.js
@@ -1,20 +1,61 @@
 import express from "express";
 import User from '../models/user.model.js';
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
 
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
-    const user = await User.findOne({ email: email });
+    // const user = await User.findOne({ email: email });
 
-    if (user && user.password === password)
-        res.send({
-            message: `Login successful`,
-            user: user,
-        });
-    else res.send({ message: `Incorrect username/password` });
+    // if (user && user.password === password)
+    //     res.send({
+    //         message: `Login successful`,
+    //         user: user,
+    //     });
+    // else res.send({ message: `Incorrect username/password` });
+
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            bcrypt.compare(req.body.password, user.password)
+                .then((passwordCheck) => {
+                    if (!passwordCheck) {
+                        return res.status(400).send({
+                            message: `Incorrect password`,
+                            error
+                        });
+                    }
+                    const token = jwt.sign({
+                        userId: user._id,
+                        userEmail: user.email
+                    },
+                        "RANDOM_TOKEN",
+                        { expiresIn: "24h" }
+                    );
+
+                    res.status(200).send({
+                        message: `Login successful`,
+                        email: user.email,
+                        user: user,
+                        token
+                    })
+                })
+                .catch((e) => {
+                    res.status(400).send({
+                        message: `Incorrect password`,
+                        e
+                    })
+                })
+        })
+        .catch((e) => {
+            res.status(404).send({
+                message: `User not found`,
+                e
+            })
+        })
 
 });
 
--- backend/src/routes/register.route.js
+++ backend/src/routes/register.route.js
@@ -1,14 +1,31 @@
 import express from "express";
 import User from '../models/user.model.js';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const user = new User(req.body);
+    //const user = new User(req.body);
 
-    user.save()
-        .then(() => res.send({ message: "Registration successful" }))
-        .catch(() => res.send({ message: 'This user already exists' }))
+    bcrypt
+        .hash(req.body.password, 10)
+        .then((hashedPassword) => {
+            const user = new User({
+                name: req.body.name,
+                handle: req.body.handle,
+                email: req.body.email,
+                password: hashedPassword,
+            });
+            user.save()
+                .then((result) => res.send({ message: "Registration successful", result }))
+                .catch(() => res.send({ message: 'This user already exists' }))
+        })
+        .catch((e) => {
+            res.status(500).send({
+                message: "Password was not hashed successfully",
+                e
+            });
+        });
 });
 
 export { router as register };
